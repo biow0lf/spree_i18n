a7754df721bb92e728a6a60ec5dc187c73cce145
Washington Luiz <huoxito@gmail.com>
2013-04-16 03:08:57 -0300

    Introduce I18n options in frontend header

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,4 +1,4 @@
 Spree::AppConfiguration.class_eval do
-  preference :all_locales, :array, :default => ['en', 'es', 'de']
-  preference :supported_locales, :array, :default => ['en']
+  preference :all_locales, :array, :default => ['en', 'es', 'de', 'pt-BR']
+  preference :supported_locales, :array, :default => ['en', 'es', 'de', 'pt-BR']
 end
